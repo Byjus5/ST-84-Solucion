4a2cb685ddccca4f2cbb8483f6922876ca01587c
Byjus5 <89545408+Byjus5@users.noreply.github.com>
2021-12-05 10:07:00 -0600

    Update StoryCard.js

--- screens/StoryCard.js
+++ screens/StoryCard.js
@@ -43,7 +43,7 @@ export default class StoryCard extends Component {
         <TouchableOpacity
           style={styles.container}
           onPress={() =>
-            this.props.navigation.navigate("StoryScreen", {
+            this.props.navigation.navigate("PantalladeHistoria", {
               story: this.props.story
             })
           }
